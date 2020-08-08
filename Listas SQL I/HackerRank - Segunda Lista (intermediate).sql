1581ce5078600febe7dad474870011ed1152edf8
luonishicastro <luonishcastro@gmail.com>
2020-08-07 22:59:12 -0300

    Upload Segunda Lista

--- Listas SQL I/HackerRank - Segunda Lista (intermediate).sql
+++ Listas SQL I/HackerRank - Segunda Lista (intermediate).sql
@@ -85,3 +85,5 @@ ORDER BY [occupation_count], [occupation];
 
 
 --=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=
+/* Print Prime Numbers - Write a query to print all prime numbers less than or equal to 1000. Print your result on a single line, and use the ampersand (&) character as your separator (instead of a space). Output Example: 2&3&5&7 */
+
